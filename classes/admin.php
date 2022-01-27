29c228e57a96aeaa52aa4a70712bd1ec99c70f49
MohammedSalahadin <mohammed.salahadinf18@komar.edu.iq>
2022-01-27 19:45:34 +0300

    first commit

---
+++ classes/admin.php
@@ -0,0 +1,73 @@
+<?php
+//include database
+
+class Admin{
+    //admin info
+    public $userName;
+    public $password;
+    public $email;
+    public $firstName;
+    public $lastName;
+    public $domainName;
+    //general info
+    public $localURL;
+    public $hideHomePageMenueBar = false;
+    public $returnUrlOnLogout;
+    public $dispachPhoneNumber;
+    public $dispachPhoenNumberGuards;
+    public $timeZone;
+    public $contactCompanyName;
+    public $contactAddress;
+    public $city;
+    public $states_id;
+    public $contactEmail;
+    public $contactPhoneNumber;
+    public $handheldPhotoTimestampText;
+    public $renderHomePageAsHTMLMarkup;
+    public $IncludeArrivals_DeparturesInDAR;
+    public $HomePageMessage;
+    public $MobileDeviceLoginMessage;
+    public $hideDropDownCitySelector;
+    public $propertyFindExampleText;
+    public $externalUrlLinks;
+    //notification info
+    public $ResendNotificationAlertForUnacknowledgedIssuesPriority1;
+    public $ResendNotificationAlertForUnacknowledgedIssuesPriority2;
+    public $ResendNotificationAlertForUnacknowledgedIssuesPriority3;
+    //logos info
+    public $mainPageLogo;
+    public $reportHeaderLogo;
+    //email info
+    public $fromEmailAddressNewOrder;
+    public $fromEmailAddressNewIssue;
+    public $fromEmailAddressAppeals;
+    public $SystemEmailNotification;
+
+    public $devices = array();
+    public $users = array();
+    public $property = array();
+    public $issueTypes = array();
+
+    
+    public function __construct() {}
+
+    
+    //create New admin, should provide basic info
+    public function createAdmin($userName, $password,$email,$firstName,$lastName,$domainName){
+        
+    }
+
+    public function getAdmin(){}
+    
+
+}
+
+
+
+
+
+
+
+
+
+?>
