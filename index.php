b63d3afbf43c56656762472d2e57cac508cedc54
MohammedSalahadin <mohammed.salahadinf18@komar.edu.iq>
2022-02-15 21:59:37 +0300

    Changed falcon to falcontrac

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/styles/styles.css">
-    <title>Falcon</title>
+    <title>Falcontrac</title>
 </head>
 
 <body>
     <div class="container py-4">
         <div class="d-flex justify-content-between">
-            <h1>Falcon</h1>
+            <h1>Falcontrac</h1>
             <ul class="nav">
                 <li class="nav-item">
                     <a class="nav-link" href="#">test</a>
