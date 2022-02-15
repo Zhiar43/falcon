89c94c356276b013f05e04ab3eb6023555acd952
ahmadsoran <ahmadsorann@gmail.com>
2022-02-15 10:25:45 -0800

    first init

--- index.php
+++ index.php
@@ -1,46 +1,56 @@
-<?php
-require("headerTAGS.php")
-?>
+<!DOCTYPE html>
+<html lang="en">
 
-<div class="container py-4">
-    <div class="d-flex justify-content-between">
-        <h1>Falcon</h1>
-        <ul class="nav">
-            <li class="nav-item">
-                <a class="nav-link" href="#">test</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">test</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">test</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link">test</a>
-            </li>
-        </ul>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/styles/styles.css">
+    <title>Falcon</title>
+</head>
+
+<body>
+    <div class="container py-4">
+        <div class="d-flex justify-content-between">
+            <h1>Falcon</h1>
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">test</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">test</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">test</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">test</a>
+                </li>
+            </ul>
+        </div>
+        <div class="d-flex justify-content-center pt-5">
+
+            <form class="bg-light p-5 rounded shadow w-75" action="routes/monitor.php">
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">username</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
     </div>
-    <div class="d-flex justify-content-center pt-5">
 
-        <form class="bg-light p-5 rounded shadow w-75" action="routes/monitor.php">
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">username</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-    </div>
-</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
 
-<?php
-require("footerTAGS.php")
-?>
+</html>
