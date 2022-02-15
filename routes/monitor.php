2864b24901d6f75532a99417e2e0a7e337e8d595
ahmadsoran <ahmadsorann@gmail.com>
2022-02-15 10:26:00 -0800

    init

--- routes/monitor.php
+++ routes/monitor.php
@@ -1,13 +1,23 @@
-<?php
-require("../headerTAGS.php")
-?>
+<!DOCTYPE html>
+<html lang="en">
 
-<div class="container py-5">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/styles/styles.css">
+    <title>Falcon</title>
+</head>
 
-    <h1>test</h1>
-</div>
+<body>
+    <div class="container height-full d-flex justify-content-center aligin-item-center">
+
+        <h1 class="bg-light p-3 rounded ">test</h1>
+    </div>
 
 
-<?php
-require("../footerTAGS.php")
-?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
