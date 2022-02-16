28b03e8f88a8fabb616db9c5758121a468081251
ahmadsoran <ahmadsorann@gmail.com>
2022-02-16 11:34:23 -0800

    admin dashboard nav

---
+++ components/navbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-light px-5 " >
+  <div class="container-fluid">
+    <a class="navbar-brand logo"><img src="../assets/images/logo.png" alt=""></a>
+    <div>
+        <div class="d-flex my-3">
+
+            <button class="btn mx-3 rounded btn-primary ">
+                issue monitor
+            </button>
+            <button class="btn mx-3 rounded btn-primary">
+                logout
+            </button>
+            <button class="btn mx-3 rounded btn-primary">
+                help
+            </button>
+        </div>
+        <div class="LoggedinAdminName mx-3">
+        <p>USer: (A) The admin Name</p>
+        <p>ID: 123456</p>
+        </div>
+    </div>
+  </div>
+</nav>
