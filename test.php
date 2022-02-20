09f39779fe2938b1efd2a647e3012c25fd6cb269
MohammedSalahadin <mohammed.salahadinf18@komar.edu.iq>
2022-02-20 22:51:53 +0300

    few edits

--- test.php
+++ test.php
@@ -3,32 +3,7 @@
 
 
 $dayTime = array(
-    'sun' => array(
-        '12' => true,
-        '1' => false,
-        '2' => false,
-        '3' => false,
-        '4' => false,
-        '5' => false,
-        '6' => false,
-        '7' => false,
-        '8' => false,
-        '9' => false,
-        '10' => false,
-        '11' => false,
-        '12' => false,
-        '13' => false,
-        '14' => false,
-        '15' => false,
-        '16' => false,
-        '17' => false,
-        '19' => false,
-        '20' => false,
-        '21' => false,
-        '22' => false,
-        '23' => false,
-        '24' => false,
-    ),
+    ,
     'mon' => array(
         '12' => true,
         '1' => false,
@@ -57,7 +32,6 @@ $dayTime = array(
     ),
 );
 
-
 $sun = array(
     '12' => true,
     '1' => false,
@@ -85,8 +59,10 @@ $sun = array(
     '24' => false,
 );
 
+
 $encoded = json_encode($dayTime);
 
+echo $encoded;
 
 
 
