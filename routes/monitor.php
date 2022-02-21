3ad3c7e24e011e30364de070763d249dc668462d
ahmadsoran <ahmadsorann@gmail.com>
2022-02-21 22:16:55 +0300

    reorganize file codes

--- routes/monitor.php
+++ routes/monitor.php
@@ -2,30 +2,24 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="../assets/styles/styles.css">
-    <!-- change row color on hover -->
-    <style>
-      .table-hover tbody tr:hover td, .table-hover tbody tr:hover th {
-        background-color: #ffd942;
-        cursor: pointer;
-      }
-    </style>
-    <title>Falcontrac</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="../assets/styles/styles.css">
+  <title>Falcontrac</title>
 </head>
 
 <body>
   <?php require('../components/navbar.php') ?>
   <div class="container">
-<div class="p-1 rounded text-center mt-3 " style="background-color: rgb(216, 216, 216);">
+    <div class="p-1 rounded text-center mt-3 " style="background-color: rgb(216, 216, 216);">
 
-  <h1 class="" style="color: gray;">
-ISSUE MONITOR</h1>
-</div>
+      <h1 class="" style="color: gray;">
+        ISSUE MONITOR</h1>
+    </div>
     <div class="d-flex justify-content-center ">
       <?php 
   require('../components/monitor-table/monitorTable.php')
@@ -34,7 +28,9 @@ ISSUE MONITOR</h1>
 
   </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+    crossorigin="anonymous"></script>
 </body>
 
 </html>
