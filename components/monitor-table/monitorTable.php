c60977fd73b628c864f597ec0a2705b431fca415
ahmadsoran <ahmadsorann@gmail.com>
2022-02-21 22:17:08 +0300

    reorganize file codes

--- components/monitor-table/monitorTable.php
+++ components/monitor-table/monitorTable.php
@@ -3,49 +3,50 @@
     <tr>
       <th scope="col">Issue ID</th>
       <th scope="col">
-<select class="form-select bg-light"   aria-label="property name">
-        <option  selected disabled>Property Name</option>
-     
-    </select>
+        <select class="form-select bg-light" aria-label="property name">
+          <option selected disabled>Property Name</option>
+
+        </select>
       </th>
-      
+
       <th scope="col">
 
-    <select class="form-select bg-light"   aria-label="property name">
-        <option  selected disabled>Reported Issue</option>
-        <option value="1">test</option>
-  
-    </select>
+        <select class="form-select bg-light" aria-label="property name">
+          <option selected disabled>Reported Issue</option>
+          <option value="1">test</option>
+
+        </select>
       </th>
       <th scope="col">
-        <input type="date" class="form-select bg-light" name="createdDate" id="">
+
+        <input type="datetime-local" class="form-select bg-light" name="createdDate" id="">
       </th>
 
       <!-- choose user modal filter start -->
       <th scope="col">
-      <button class="btn bg-light" data-bs-toggle="modal" data-bs-target="#exampleModal">Created By</button>
-      <?php 
+        <button class="btn bg-light" data-bs-toggle="modal" data-bs-target="#exampleModal">Created By</button>
+        <?php 
       require('monitorFilterModal.php')
       ?>
-        </th>
-        <!-- modal filter end -->
+      </th>
+      <!-- modal filter end -->
 
-<th scope="col">
-      <select class="form-select bg-light"   aria-label="property name">
-        <option  selected disabled>All</option>
-        <option value="1">test</option>
-    </select>
-        </th>
+      <th scope="col">
+        <select class="form-select bg-light" aria-label="property name">
+          <option selected disabled>All</option>
+          <option value="1">test</option>
+        </select>
+      </th>
 
-<!-- choose user modal filter start -->
-<th scope="col">
-      <button class="btn bg-light" data-bs-toggle="modal" data-bs-target="#exampleModal">Assigned</button>
-        </th>
-        <!-- modal filter end -->
+      <!-- choose user modal filter start -->
+      <th scope="col">
+        <button class="btn bg-light" data-bs-toggle="modal" data-bs-target="#exampleModal">Assigned</button>
+      </th>
+      <!-- modal filter end -->
     </tr>
   </thead>
-  <tbody>
-<?php require('tableBody.php') ?>
+  <tbody class="text-center fs-6">
+    <?php require('tableBody.php') ?>
   </tbody>
 </table>
 
--- components/monitor-table/tableBody.php
+++ components/monitor-table/tableBody.php
@@ -1,10 +1,20 @@
 <tr>
-      <th scope="row">123456 <span class="tableCameraImg" data-bs-toggle="modal" data-bs-target="#cameraModal" >
+  <th scope="row">123456 <span class="tableCameraImg" data-bs-toggle="modal" data-bs-target="#cameraModal">
       <i class="bi bi-camera"></i></span></th>
-      <td>Austin American Statesman</td>
-      <td class="property-bg"> property inspection</td>
-      <td>Wed 2/16 11:14 AM</td>
-      <td>MARKELLEJ</td>
-      <td>3</td>
-      <td class="assign-bg">MARKELLEJ</td>
-    </tr>
+  <td>Austin American Statesman</td>
+  <td class="issue-active"> property inspection</td>
+  <td>Wed 2/16 11:14 AM</td>
+  <td>MARKELLEJ</td>
+  <td>3</td>
+  <td class="issue-unactive">MARKELLEJ</td>
+</tr>
+<tr>
+  <th scope="row">123456 <span class="tableCameraImg" data-bs-toggle="modal" data-bs-target="#cameraModal">
+      <i class="bi bi-camera"></i></span></th>
+  <td>Austin American Statesman</td>
+  <td class="issue-active"> property inspection</td>
+  <td>Wed 2/16 11:14 AM</td>
+  <td>MARKELLEJ</td>
+  <td>3</td>
+  <td class="issue-active">MARKELLEJ</td>
+</tr>
