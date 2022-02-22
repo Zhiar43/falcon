db3ad8df38d63827b8e1c2e81c36f2027423bd06
ahmadsoran <ahmadsorann@gmail.com>
2022-02-23 00:44:02 +0300

    modal created

---
+++ components/monitor-table/userMapModal.php
@@ -0,0 +1,18 @@
+<div class="modal fade" id="userMapModal" tabindex="-1" aria-labelledby="userMapModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg  modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="userMapModalLabel">User Map </h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body ">
+       <!-- your code here start -->
+       
+       <!-- your code here  end -->
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+</div>
