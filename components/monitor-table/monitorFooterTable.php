476dc28733cb11356fc46497266b95e8edb88de0
ahmadsoran <ahmadsorann@gmail.com>
2022-02-23 00:42:30 +0300

    add table footer modals

--- components/monitor-table/monitorFooterTable.php
+++ components/monitor-table/monitorFooterTable.php
@@ -32,16 +32,16 @@
  <div class="d-flex alert-secondary  justify-content-between aligin-item-center">
  	
  		<div>
+			 
  		<img src="https://demo2.silvertracker.net/Images/summarysm.png">
 		<img src="https://demo2.silvertracker.net/Images/vehiclesearch.png">
  		<img src="https://demo2.silvertracker.net/Images/taskbluesm.png">
  		<img src="https://demo2.silvertracker.net/Images/checkpointbluesm.png">
- 		<img src="https://demo2.silvertracker.net/Images/gps.png">
- 		<img src="https://demo2.silvertracker.net/Images/messageboardsm.png">
- 		<img src="https://demo2.silvertracker.net/Images/NewNote.png">
+ 		<img data-bs-toggle="modal" data-bs-target="#userMapModal" src="https://demo2.silvertracker.net/Images/gps.png">
+ 		<img id="monitorMessageIcon" src="https://demo2.silvertracker.net/Images/messageboardsm.png">
+ 		<img data-bs-toggle="modal" data-bs-target="#propertyAlertModal" src="https://demo2.silvertracker.net/Images/NewNote.png">
  		</div>
- 	
-
+		 
  		<div class="text-center ">
  		<input type="button" id="cmdnew" value="New Action" style="background-color: #03508e; color: white;">
  	</div>
@@ -60,3 +60,5 @@
 </div>
 
 
+			 <?php require('userMapModal.php') ?>
+			 <?php require('propertyAlertModal.php') ?>
