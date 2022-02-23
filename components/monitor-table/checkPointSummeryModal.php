6124a4a30da59f15806d5a4eda37dca1a8854b5f
ahmadsoran <ahmadsorann@gmail.com>
2022-02-23 11:44:16 +0300

    all footer modal created

---
+++ components/monitor-table/checkPointSummeryModal.php
@@ -0,0 +1,19 @@
+<div class="modal fade" id="checkPointSummery" tabindex="-1" aria-labelledby="checkPointSummeryLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg  modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="checkPointSummeryLabel">CheckPoint Summery</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body ">
+       <!-- your code here start -->
+       
+       <!-- your code here  end -->
+     
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+</div>
